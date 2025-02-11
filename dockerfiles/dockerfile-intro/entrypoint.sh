809d2d3b0f158f6e2c3e050afc0d40ff267d17d9
Przemek Nowak <przemek.nowak.pl@gmail.com>
2025-02-11 18:29:54 +0100

    feat: commended code

--- dockerfiles/dockerfile-intro/entrypoint.sh
+++ dockerfiles/dockerfile-intro/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Starting container..."
 echo "Message: $MESSAGE"
+# echo "Hello from IMAGE_NAME=$IMAGE_NAME"
 
 if [ "$1" = "server" ]; then
     echo "Running a simple Python HTTP server on port $PORT..."
